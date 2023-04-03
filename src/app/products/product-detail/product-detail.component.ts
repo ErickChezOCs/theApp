00efdb57e21c6c968c9ee98e718363a9bbdfb881
ErickChezOCs <nounga72@yahoo.fr>
2023-04-03 20:37:04 +0200

    viewEncapsulation

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,21 +1,41 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'; //Input sert à passer des données d'un composant
- // parent à un composant enfant
-
+import { Component, Input, Output, EventEmitter, ViewEncapsulation, OnChanges, SimpleChanges,
+  ChangeDetectionStrategy} from '@angular/core'; //Input sert à passer des données d'un composant
+ // parent à un composant enfant  OnInit,
+// ngOnInit(): void {
+//  console.log(`Name is ${this.name} in the constructor`);}
+//
 
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 
 })
 
 
-export class ProductDetailComponent {
-  @Input() name ='';
+export class ProductDetailComponent implements OnChanges {
+  constructor() {
+    console.log(`Name is ${this.name} in the constructor`);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+      const product = changes['name'];
+      const oldValue = product.previousValue;
+      const newValue = product.currentValue;
+      console.log(`Product changed from ${oldValue} to ${newValue}`);
+  }
+
+  @Input() name!:string ;
   @Output() bought = new EventEmitter();
   buy() {
     this.bought.emit();
   }
+  get productName(): string {
+    console.log(`Get ${this.name}`);
+    return this.name;
+  }
+
 
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent {
-  selectedProduct = '';
+  selectedProduct = 'Microphone';
   onBuy() {
     window.alert(`You just Bought ${this.selectedProduct} !`);
   }
